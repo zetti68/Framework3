1c6d031a524768766ad1269ef9f314aaee46fbdf
Torsten Zetzsche <torsten.zetzsche@siemens.com>
2015-01-22 08:08:13 +0100

    new: framework ITextIOS separated

---
+++ Versions/A/Headers/ITextIOS.h
@@ -0,0 +1,13 @@
+//
+//  iTextIOS.h
+//  iTextIOS
+//
+//  Created by Torsten Zetzsche on 18.09.14.
+//  Copyright (c) 2014 iText. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iTextIOS : NSObject
+
+@end
